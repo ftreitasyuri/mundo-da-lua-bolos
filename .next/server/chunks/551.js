9a1ab7b61a3736d644a73930fbe2b2ce3495c27c
Yuri Freitas Queiroz <yuri.queiroz@totvs.com.br>
2023-03-21 12:16:31 -0300

    7º upload

---
+++ .next/server/chunks/551.js
@@ -0,0 +1,127 @@
+exports.id = 551;
+exports.ids = [551];
+exports.modules = {
+
+/***/ 6264:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"classNav": "Cabecalho_classNav__RTnLg",
+	"logotipo": "Cabecalho_logotipo__Uvkau",
+	"flexdiv": "Cabecalho_flexdiv__TsNUD",
+	"links": "Cabecalho_links__mU8bF"
+};
+
+
+/***/ }),
+
+/***/ 8710:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"app": "Home_app__OR_3m",
+	"containerPrincipal": "Home_containerPrincipal__hStAY",
+	"separacao": "Home_separacao__1OpwV"
+};
+
+
+/***/ }),
+
+/***/ 3621:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"rodape": "Rodape_rodape__hoSPN"
+};
+
+
+/***/ }),
+
+/***/ 402:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (/* binding */ Cabecalho)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(1664);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _styles_Cabecalho_module_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(6264);
+/* harmony import */ var _styles_Cabecalho_module_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_styles_Cabecalho_module_css__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+function Cabecalho() {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        className: (_styles_Cabecalho_module_css__WEBPACK_IMPORTED_MODULE_2___default().classNav),
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                    className: (_styles_Cabecalho_module_css__WEBPACK_IMPORTED_MODULE_2___default().logotipo)
+                })
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                id: (_styles_Cabecalho_module_css__WEBPACK_IMPORTED_MODULE_2___default().flexdiv),
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                            className: (_styles_Cabecalho_module_css__WEBPACK_IMPORTED_MODULE_2___default().links),
+                            href: "/",
+                            children: "Home"
+                        })
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                            className: (_styles_Cabecalho_module_css__WEBPACK_IMPORTED_MODULE_2___default().links),
+                            href: "/contatos",
+                            children: "Contatos"
+                        })
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                            className: (_styles_Cabecalho_module_css__WEBPACK_IMPORTED_MODULE_2___default().links),
+                            href: "/quemSomos",
+                            children: "Quem Somos"
+                        })
+                    })
+                ]
+            })
+        ]
+    });
+}
+
+
+/***/ }),
+
+/***/ 1833:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (/* binding */ Rodape)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _styles_Rodape_module_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3621);
+/* harmony import */ var _styles_Rodape_module_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_Rodape_module_css__WEBPACK_IMPORTED_MODULE_1__);
+
+
+function Rodape() {
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+        className: (_styles_Rodape_module_css__WEBPACK_IMPORTED_MODULE_1___default().rodape),
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+            children: "RODAPE"
+        })
+    });
+}
+
+
+/***/ })
+
+};
+;
